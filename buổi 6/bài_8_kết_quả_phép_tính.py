87a0a51e32c569ed1439c3bc33c2d621c9d480c4
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-08-24 13:44:12 +0700

    Update bài_8_kết_quả_phép_tính.py

--- buổi 6/bài_8_kết_quả_phép_tính.py
+++ buổi 6/bài_8_kết_quả_phép_tính.py
@@ -2,7 +2,7 @@
 import math
 
 #1
-A = pow(32,0.2) - pow(1/64, -0.25) + pow(8/27,1/3)
+A = math.pow(32,0.2) - math.pow(1/64, -0.25) + math.pow(8/27,1/3)
 print("A = ",float((round(A,3))))
 #2
 a = float(input("Nhập a = "))
@@ -11,7 +11,7 @@ phan_so1 = (math.sqrt(a) - math.sqrt(b)) / (math.pow(a,1/4) - math.pow(b, 1/4))
 phan_so2 = (math.sqrt(a) + math.pow(a*b,1/4)) / (math.pow(a,1/4) + math.pow(b, 1/4))
 B = phan_so1 - phan_so2
 
-if float(round(B,4)) == float(round(pow(b,1/4),4)):
+if float(round(B,4)) == float(round(math.pow(b,1/4),4)):
     print("Kết quả đúng = ",float(round(B,4)))
 else:
     print("Kết quả sai")
