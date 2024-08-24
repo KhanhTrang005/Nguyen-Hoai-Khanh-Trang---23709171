01ec117286f656e186b5b7def292eb3cca33297d
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-08-24 13:38:10 +0700

    Add files via upload

---
+++ buổi 6/bài_2.py
@@ -0,0 +1,9 @@
+﻿"Nguyễn Hoài Khánh Trang - 23709171"
+
+a= int(input("Nhập a = "))
+b = int(input("Nhập b = "))
+
+print ('Tổng = ', a+b )
+print ('Hiệu  = ', a-b)
+print ('Tích  = ', a*b)
+print ('Thương  = ', round(a/b, 3))
